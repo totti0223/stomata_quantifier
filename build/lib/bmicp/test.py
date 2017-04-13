711f78f96f0288e30d418d74ed99e652c4a10c5d
totti0223 <tyosuke@aquaseerser.com>
2017-04-14 08:45:21 +0900

    delete unrelated files

--- build/lib/bmicp/test.py
+++
@@ -1,3 +0,0 @@
-import bm_icp
-
-bm_icp.bmicp("/Users/todayousuke/Desktop/objectdetection/bm_icp_v2/bm_icp/config.ini", "/Users/todayousuke/Desktop/objectdetection/bm_IcP/sample/20160524_ITbM-DMSO_3rd_03.jpg")
